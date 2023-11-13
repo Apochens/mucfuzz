56ce599cb41b7845eb3974af86f11b58823791bd
Apochens <15309036477@qq.com>
2023-11-13 05:14:06 +0000

    upgrade Go

--- build/install_tools.sh
+++ build/install_tools.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-#wllvm and gllvm
-pip3 install --upgrade pip==9.0.3
+# wllvm and gllvm
+# pip3 install --upgrade pip==9.0.3
 pip3 install wllvm
-go get github.com/SRI-CSL/gllvm/cmd/...
+GO111MODULE=off go get github.com/SRI-CSL/gllvm/cmd/...
 
 #llvm-diff-parmesan
 #(cd tools/llvm-diff-parmesan && mkdir build && cd build && cmake .. && cmake --build . && cp llvm-diff-parmesan ../../../bin/)
